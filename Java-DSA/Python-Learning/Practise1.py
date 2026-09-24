d8491381df80d658650430aa691517c3d9703fb4
Ankurkashyap-Coder <ankurkashyap2006@outlook.com>
2026-09-24 11:47:42 +0530

    Delete Java-DSA/Python-Learning directory

--- Java-DSA/Python-Learning/Practise1.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter the first number"))
-b = int(input("Enter the second number"))
-c = int(input("Enter the third number"))
-d = int(input("Enter the fourth number"))
-
-print("Your sum is : " ,  a + b + c + d)
